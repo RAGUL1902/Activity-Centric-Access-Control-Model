02d02f32982a4311e9922ddf538f8981452d2c7e
naveenvenk17 <naveenvenkat1711@gmail.com>
2022-11-17 15:33:49 +0530

    Update Main

--- policy_helper.py
+++ policy_helper.py
@@ -25,7 +25,7 @@ class PolicyHelper:
                 i[idx] = i[idx].strip("(),")
             new_policy.policy_number = int(i[0])
             new_policy.object_name = i[2]
-            new_policy.source_name = i[3]
+            new_policy.state_name = i[3]
             new_policy.activity_name = i[4]
             new_policy.pre_condition = list([i[5], i[6], i[7]])
             new_policy.current_condition = list([i[8], i[9], i[10]])
@@ -41,6 +41,8 @@ class PolicyHelper:
         self.update_policies_file()
 
     def delete_policy(self, policy_number):
+
+        #TODO check policy exist before deleting
         print(f"Deleting policy number: {policy_number}...")
         new_policy_list = []
         counter = 1
@@ -52,8 +54,34 @@ class PolicyHelper:
         self.policy_list = new_policy_list
         self.update_policies_file()
 
-    def check_policy(self, machine, user, state):
+    def check_policy(self, machine, old_state,new_state, machines_list):
 
+        for i in self.policy_list:
+            if (i.name == machine & i.state_name == old_state ):
+                pre_condition = i.pre_condition
+                current_condition = i.current_condition
+                post_condition = i.post_condition
+
+                pre = False
+                curr = False
+                post = False
+
+                for j in machines_list :
+                    if (j.name == pre_condition[0] & j.state_name == pre_condition[1]):
+                        pre = True
+                    if (j.name == current_condition[0] & j.state_name == current_condition[1]):
+                        curr = True
+                    if (j.name == post_condition[0] & j.state_name == post_condition[1]):
+                        post = True
+                if( pre == True & curr == True & post == True):
+                    
+                    for j in machines_list:
+                        if (j.name ==pre_condition[0] & j.state_name ==pre_condition[1]):
+                            j.state_name = new_state
+                    print('\n->MACHINE STATE CHANGED\n')
+
+                else :
+                    print("\n POLICY DOESN'T ALLOW THIS CHANGE")
         pass
 
     def show_policies(self):
@@ -69,7 +97,7 @@ class PolicyHelper:
             pre_condition = "(),"
             current_condition = "(),"
             post_condition = "()"
-            object_info = f"({i.object_name}, {i.source_name}, {i.activity_name}),"
+            object_info = f"({i.object_name}, {i.state_name}, {i.activity_name}),"
 
             if len(i.pre_condition)>0:
                 pre_condition = f"({i.pre_condition[0]}, {i.pre_condition[1]}, {i.pre_condition[2]}),"              
@@ -81,3 +109,5 @@ class PolicyHelper:
             policy_string = f"{i.policy_number}) < {object_info} {pre_condition} {current_condition} {post_condition} > \n"
             f.write(policy_string)
         f.close()
+
+
