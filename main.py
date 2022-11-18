5180ec2adbbac1b1ace9b3d72ab44d59d5687573
Ragul1902 <ragulsenthilkumar02@gmail.com>
2022-11-18 23:37:15 +0530

    Remove Duplicate machine objects

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import time
 from machines import Machine
 import constants
-#from constants import machines_list
+from constants import machines_list
 from policy_helper import PolicyHelper
 
 # TODO help function
@@ -11,37 +11,71 @@ os.system('cls')
 
 POLICY_FILE = 'policies.txt'
 # creating objects
-crusher_and_grinder_1 = Machine(
-    'crusher_and_grinder_1', "OFF", "breakdown_chemicals")
-agitator_1 = Machine('agitator_1', "OFF", "agitate")
-suction_1 = Machine('suction_1', "OFF", 'remove_gas')
-suction_2 = Machine('suction_2', "OFF", 'remove_gas')
-pump_1 = Machine("pump_1", "OFF", 'pump_chemicals')
-pump_2 = Machine("pump_2", "OFF", 'pump_chemicals')
-mixer_1 = Machine("mixer_1", "OFF", "mix")
-mixer_2 = Machine("mixer_2", "OFF", "mix")
-mixer_3 = Machine("mixer_3", "OFF", "mix")
-high_shear_mixer_1 = Machine(
-    "high_shear_mixer_1", "OFF", 'mix_with_high_shear')
-conveyer_1 = Machine("conveyer_1", "OFF", 'move_chemicals')
-compressor_1 = Machine("compressor_1", "OFF", "compress")
-tank_1 = Machine("tank_1", "OFF", 'store')
-packing_machine_1 = Machine('packing_machine_1', "OFF", "pack")
-air_conditioner_1 = Machine("air_conditioner_1", "OFF", 'air_conditioning')
+# crusher_and_grinder_1 = Machine(
+#     'crusher_and_grinder_1', "OFF", "breakdown_chemicals")
+# agitator_1 = Machine('agitator_1', "OFF", "agitate")
+# suction_1 = Machine('suction_1', "OFF", 'remove_gas')
+# suction_2 = Machine('suction_2', "OFF", 'remove_gas')
+# pump_1 = Machine("pump_1", "OFF", 'pump_chemicals')
+# pump_2 = Machine("pump_2", "OFF", 'pump_chemicals')
+# mixer_1 = Machine("mixer_1", "OFF", "mix")
+# mixer_2 = Machine("mixer_2", "OFF", "mix")
+# mixer_3 = Machine("mixer_3", "OFF", "mix")
+# high_shear_mixer_1 = Machine(
+#     "high_shear_mixer_1", "OFF", 'mix_with_high_shear')
+# conveyer_1 = Machine("conveyer_1", "OFF", 'move_chemicals')
+# compressor_1 = Machine("compressor_1", "OFF", "compress")
+# tank_1 = Machine("tank_1", "OFF", 'store')
+# packing_machine_1 = Machine('packing_machine_1', "OFF", "pack")
+# air_conditioner_1 = Machine("air_conditioner_1", "OFF", 'air_conditioning')
 
-machines_list = [crusher_and_grinder_1, agitator_1, suction_1, suction_2, pump_1, pump_2, mixer_1, mixer_2, mixer_3,
-                 high_shear_mixer_1, conveyer_1, compressor_1, tank_1, packing_machine_1, air_conditioner_1]
+# machines_list = [crusher_and_grinder_1, agitator_1, suction_1, suction_2, pump_1, pump_2, mixer_1, mixer_2, mixer_3,
+#                  high_shear_mixer_1, conveyer_1, compressor_1, tank_1, packing_machine_1, air_conditioner_1]
 
+from tkinter import *
+from tkinter import messagebox
+from importlib import reload
+# from constants import machines_list
+master = Tk()
 
-policyHelper = PolicyHelper(constants.POLICY_FILE)
+master.title("Activity Centric Access Control Model")
+def show_state():
+   for index, machine_iterator in enumerate(machines_list):
+      Label(master,text=machine_iterator.name).grid(row=index+1)
+      Label(master,text="----->").grid(row=index+1,column=1)
+      Label(master,text=machine_iterator.state_name).grid(row=index+1,column=2)
+#       machine_iterator.show_state()
+#    print("=========================================================================\n")
+#    master.after(1000, show_state)
+
+def show_policy():
+    policy = Text(master, height=100, width=100)
+    #policy.pack()
+    policy.grid(column=5)
+    filename='policies.txt'
+    with open(POLICY_FILE, 'r') as f:
+      policy.insert(END, f.read())
+    # master.after(1000, show_policy)
+
+# reload(constants)
+show_state()
+show_policy()
+mainloop()
+
+# ==========================================================================================================================================
+
+policyHelper = PolicyHelper(POLICY_FILE)
 print("\n\n ---------------------- COMMANDS-------------------")
 print('\n show_policies() - show policies')
 print('\n show_activity() - show current activity status')
 print('\n help() - to print all command')
 print("\n\n --------------------------------------------------")
 
-time.sleep(2)
+# time.sleep(2)
 while 1:
+
+    show_state()
+    show_policy()
 
     print("\n Enter :")
     print("\n '1' for Policy Related")
@@ -172,3 +206,7 @@ while 1:
     time.sleep(5)
 
     os.system('cls')
+
+
+# =======================================================================================================
+
