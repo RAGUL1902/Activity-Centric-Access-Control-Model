f946adf91ee3ac39f4ce4745c7e54644bdd2f69f
Ragul1902 <ragulsenthilkumar02@gmail.com>
2022-11-19 01:39:26 +0530

    Remove test and sample files

--- policy_helper_test.py
+++
@@ -1,9 +0,0 @@
-from policy_helper import PolicyHelper
-
-
-policy_helper = PolicyHelper("policies.txt")
-policy_helper.add_policy('a1', 'b1', 'c1', ['d1', 'e1', 'f1'], [], ['g1', 'h1', 'i1'])
-policy_helper.add_policy('a2', 'b2', 'c2', ['d2', 'e2', 'f2'], [], ['g2', 'h2', 'i2'])
-policy_helper.show_policies()
-policy_helper.delete_policy(1)
-policy_helper.show_policies()
