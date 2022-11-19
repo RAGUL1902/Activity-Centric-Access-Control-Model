3ed621b15214e069c157474b3c29f71c6f2a6c70
naveenvenk17 <naveenvenkat1711@gmail.com>
2022-11-19 21:25:49 +0530

    change comandline workflow

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from threading import Thread
 import tkinter
 import tkinter.messagebox
 import customtkinter
+import sys
 
 # TODO help function
 # os.system('cls')
@@ -89,137 +90,54 @@ class App(customtkinter.CTk):
 
 def take_input(machines_list=constants.machines_list):
     global policyHelper
-    print("\n\n ---------------------- COMMANDS-------------------")
-    print('\n show_policies() - show policies')
-    print('\n show_activity() - show current activity status')
-    print('\n help() - to print all command')
-    print("\n\n --------------------------------------------------")
+    print("\n---------------------- COMMANDS-------------------")
+    print('\nget_policies')
+    print('delete_policy <policy_number>')
+    print('add_policy')
+    print('help()')
+    print("--------------------------------------------------")
 
     while 1:
 
-        print("\n Enter :")
-        print("\n '1' for Policy Related")
-        print("\n '2' for Activity Related")
-        print("\n 'help()' to see all commands")
-        print("\n 'exit' to close the program")
+        input_command = input('\n->') 
 
-        command_1 = input("\n->")
+        command = input_command.split()
 
-        if command_1 == "exit":
+        if command[0] == "exit":
             break
 
-        if command_1 == '1':
-            print("\n-------------------  POLICY RELATED ---------------------")
-            print('\n\n')
+        elif command[0] == 'get_policies':
+            policies = policyHelper.get_policies()
+            print(policies)
+        
+        elif command[0] == 'delete_policy' :
+            policyHelper.delete_policy( command[1])
+        
+        elif command[0] == 'add_policy' :
 
-            print('\n "1" to add new policy')
-            print('\n "2" to delete policy')
-            print('\n "3" to show policy')
+            print("Enter new policy in the following format :")
+            add_policy_string = input("Machine Newstate , Pre_condition state , concurrent_Condition state , post_condition state\n")
+            
+            words = add_policy_string.split()
 
-            command_2 = input("\n->")
+            if len(words) != 11 :
+                print('Format Error')
+            words.remove(',')
+            words.remove(',')
+            words.remove(',')
 
-            if command_2 == '1':
-                print("\n-------------------  ADD POLICY ---------------------")
-                a1 = input("\n-> Enter the machine for new policy :")
-                b1 = input("\n-> Enter the New State of the machine :")
-                c1 = input("\n-> Enter the activity of the machine :")
+            policyHelper.add_policy( words[0], words[1], '-', [
+                                        words[2], words[3], '-'], [words[4], words[5], '-'], [words[6], words[7], '-'])
+            
+        elif command[0] == 'help()':
+            help()
 
-                print("\n-> ENTER PRECONDITIONS")
-                d1 = input('\n-> Enter precondition Machine name :')
-                e1 = input('\n-> Enter pre condition Machine state :')
-                f1 = input('\n-> Enter pre condition machine activity :')
-
-                print("\n-> ENTER CONCURRENT CONDITIONS")
-                j1 = input('\n-> Enter concurrent condition Machine name :')
-                k1 = input('\n-> Enter concurrent condition Machine state :')
-                l1 = input(
-                    '\n-> Enter concurrent condition machine activity :')
-
-                print("\n-> ENTER POSTCONDITIONS")
-                g1 = input('\n-> Enter post condition Machine name :')
-                h1 = input('\n-> Enter post condition Machine state :')
-                i1 = input('\n-> Enter post condition machine activity :')
-
-                PolicyHelper.add_policy(policyHelper, a1, b1, c1, [
-                                        d1, e1, f1], [j1, k1, l1], [g1, h1, i1])
-                print('\n---------------------------------------------------------')
-                pass
-            elif command_2 == '2':
-                print("\n-------------------  DELETE POLICY ---------------------")
-
-                policy_no = input(
-                    "\n-> Enter policy number you want to delete :")
-                PolicyHelper.delete_policy(policyHelper, policy_no)
-                print('\n---------------------------------------------------------')
-                pass
-            elif command_2 == '3':
-                print("\n-------------------  SHOW POLICY ---------------------")
-                PolicyHelper.get_policies(policyHelper)
-                print('\n---------------------------------------------------------')
-                pass
-            else:
-                print('INVALID COMMAND')
-                print('Enter "help" to see all commands')
-
-        elif command_1 == '2':
-            print("\n-------------------  ACTIVITY RELATED ---------------------")
-            print('\n\n ')
-
-            print('\n "1" to show all machines')
-            print('\n "2" to show current status of all machines')
-            print('\n "3" change state of a machine')
-
-            command_2 = input("\n->")
-
-            if command_2 == '1':
-
-                print("\n-------------------  LIST OF MACHINES ---------------------")
-                for machine_iterator in machines_list:
-                    print("\n" + machine_iterator.name)
-                print('\n---------------------------------------------------------')
-                pass
-
-            elif command_2 == '2':
-                print("\n-------------------  STATE OF MACHINES ---------------------")
-                for machine_iterator in machines_list:
-
-                    machine_iterator.show_state()
-                print('\n---------------------------------------------------------')
-                pass
-                pass
-            elif command_2 == '3':
-                print(
-                    "\n-------------------  CHANGE MACHINE STATE ---------------------")
-                machine_name = input("\n->Enter machine name:")
-                val = input("\n->Enter new state (ON/OFF) :")
-
-                machine_exist = False
-                for itr in machines_list:
-                    if itr.name == machine_name:
-                        machine_exist = True
-                        if (itr.state_name == val):
-                            print('\n->'+machine_name +
-                                  ' is already in ' + val + ' state')
-
-                        else:
-                            policyHelper.check_policy(
-                                machine_name, itr.state_name, val)
-                if machine_exist == False:
-                    print('\n Enter Correct Machine Name')
-
-                pass
-            else:
-                print('INVALID COMMAND')
-                print('Enter "help" to see all commands')
-            # time.sleep(5)
-
+        elif command[0] == 'cls' :
+            os.system('cls')
         else:
-            print('INVALID COMMAND')
-            print('Enter "help" to see all commands')
-            # time.sleep(5)
-
-        print("\n --------------------------next iteration----------------------------")
-
+                print('INVALID COMMAND')
+                print('Enter "help()" to see all commands')
+        
 
 
 """
